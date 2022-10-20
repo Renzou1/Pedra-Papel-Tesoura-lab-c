9a870ae6c0299fc30cacdad0be324224df0359c7
Renzou123 <114614401+Renzou1@users.noreply.github.com>
2022-10-20 17:07:32 +0000

    Pedra papel tesoura básico

--- main.c
+++ main.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
+#include<time.h>
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+int main(){
+
+  int escolha, a;
+  
+  printf("Escolha \n0. Pedra\n1. Papel\n2. Tesoura\n");
+  scanf("%d", &escolha);
+  
+  srand(time(NULL));
+  a = rand() % 3;
+  
+  // 0 = pedra, 1 = papel, 2 = tesoura.
+  
+  if (a == 0)
+    if (escolha == 0)
+      printf ("Computador escolheu pedra, empate.");
+      else if (escolha == 1)
+        printf ("Computador escolheu pedra, você venceu");
+          else if (escolha == 2)
+              printf("Computador escolheu pedra, você perdeu");
+  
+if (a == 1)
+    if (escolha == 0)
+      printf ("Computador escolheu papel, você perdeu");
+      else if (escolha == 1)
+        printf ("Computador escolheu papel, empate");
+          else if (escolha == 2)
+            printf ("Computador escolheu papel, você venceu");
+  if (a == 2)
+    if (escolha == 0)
+      printf ("Computador escolheu tesoura, você venceu");
+      else if (escolha == 1)
+        printf ("Computador escolheu tesoura, você perdeu");
+          else if (escolha == 2)
+              printf("Computador escolheu tesoura, empate");
+
+  
+
+
+
+
+
+
 }
